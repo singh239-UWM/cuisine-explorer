50fdef50dfba3c1fa34a593d4f3440f1c6a27949
Baljinder Singh <singh239@uwm.edu>
2022-02-20 13:52:33 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Router from 'next/router'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>NoTa</h1>
+      <h1 className={styles.heading}>Noni</h1>
       <p>Welcome to NoTa. Take notes</p>
       <button className={styles.button} onClick={clickHandle} >Log In</button>
     </div>
