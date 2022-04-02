9709c9b4328dece007f683c2081e276b6de07fe2
Baljinder Singh <singh239@uwm.edu>
2022-04-02 11:14:06 -0500

    Update [id].js

--- pages/countries/[id].js
+++ pages/countries/[id].js
@@ -11,7 +11,7 @@ export default function Countries({ country }) {
     const [q, setQ] = useState([])
 
 
-  
+
     useEffect(() => {
         firebaseGetCuisines(id).then((resQ) => {
             setQ(resQ)
@@ -33,10 +33,10 @@ export default function Countries({ country }) {
                 </Text>
             </Center>
             <Center m={10}>
-                {q.map((c, i) => {
-                   return <Grid id={c.id}>
-                        <Grid.Col >
-                            <div style={{ width: '70vw', minWidth: '420px', maxWidth: '900px', margin: '0'}}>
+                {q.map((c) => {
+                    return <Grid key={c.id}>
+                        <Grid.Col>
+                            <div style={{ width: '70vw', minWidth: '420px', maxWidth: '900px', margin: '0' }}>
                                 <Card shadow="sm" p="sm">
                                     <Grid>
                                         <Grid.Col span={5}>
@@ -74,44 +74,6 @@ export default function Countries({ country }) {
                         </Grid.Col>
                     </Grid>
                 })}
-                {/* <Grid>
-                    <Grid.Col >
-                        <div style={{ width: '70vw', minWidth: '420px', maxWidth: '900px', margin: '0', maxHeight: '100px' }}>
-                            <Card shadow="sm" p="sm">
-                                <Grid>
-                                    <Grid.Col span={5}>
-                                        <Card.Section>
-                                            <Image
-                                                src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-                                                height={300} />
-
-                                        </Card.Section>
-                                    </Grid.Col>
-                                    <Grid.Col span={7}>
-                                        <Group style={{ marginBottom: 5, marginTop: 2 }}>
-                                            <Title>Smosassdfsdfs sdfsdfsdf  sdfsdfsdfsdfsdfsdf</Title>
-                                        </Group>
-                                        <Group position="apart">
-                                            <Badge size="xl" color="green" variant="light">Time: 30min</Badge>
-                                            <Badge size="xl" color="yellow" variant="light">Yield: 5</Badge>
-                                        </Group>
-                                        <ScrollArea style={{ height: 165 }} scrollbarSize={16}>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis nisl dui. Nullam luctus rutrum iaculis. Donec ornare nunc quis leo aliquet, ac dapibus nisl commodo. Morbi fringilla congue sapien quis pellentesque. Quisque efficitur turpis turpis, sed congue ex accumsan eu. Pellentesque ipsum massa, interdum quis vehicula eu, rutrum a lectus. Nullam non feugiat libero. In vulputate tellus non massa auctor, in placerat sapien fringilla. Nulla facilisi. Nunc in nisi a turpis tempus maximus eu quis velit. Integer est mauris, rhoncus ac elit quis, aliquet tincidunt risus.
-                                        </ScrollArea>
-                                        <Group position="apart">
-                                            <Button variant="light" color="dark" style={{ marginTop: 14 }}>
-                                                Start Cooking
-                                            </Button>
-                                            <Button variant="light" color="indigo" style={{ marginTop: 14 }}>
-                                                Save
-                                            </Button>
-                                        </Group>
-                                    </Grid.Col>
-                                </Grid>
-                            </Card>
-                        </div>
-                    </Grid.Col>
-                </Grid> */}
             </Center>
 
 
